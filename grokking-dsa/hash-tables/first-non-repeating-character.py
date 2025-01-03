8f1607c04d40ce6f38021deb87c44fa3bc767087
Aliff Amir <aliff6406@gmail.com>
2025-01-03 23:13:16 +0000

    time and space complexity

--- grokking-dsa/hash-tables/first-non-repeating-character.py
+++ grokking-dsa/hash-tables/first-non-repeating-character.py
@@ -1,3 +1,9 @@
+""" 
+Time Complexity: O(n) each character of the string is processed once
+Space Complexity: O(1) constant space as max hashmap size will always be constant depending
+    on the constraints i.e. all lowercase english letters = 26, all ASCII characters = 128, etc.
+"""
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         # initialise an empty dict (python implements dictionary using hash table)
